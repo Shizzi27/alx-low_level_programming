a8e76a68844d28321c8137874b151e7cf1ae4ffc
Shizzi27 <shizzi27@gmail.com>
2023-08-18 16:23:00 -0700

    my second nested loop functions project

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Draws a diagonal line using _putchar
+ * @n: Number of times the character '\' should be printed
+ */
+void print_diagonal(int n)
+{
+	int spaces = 0;
+	if (n <= 0)
+	{
+	_putchar('\n');
+	return;
+	}
+
+	while (n > 0)
+	{
+
+	int i;
+
+	for (i = 0; i < spaces; i++)
+
+	_putchar(' ');
+	_putchar('\\');
+	_putchar('\n');
+	spaces++;
+	n--;
+	}
+}
