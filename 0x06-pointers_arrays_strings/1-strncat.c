a624ce2c119323c2816e664e7e8839c0ac164ed0
Shizzi27 <shizzi27@gmail.com>
2023-08-24 17:41:15 -0700

    Pointers and arrays 2nd sprint

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncat - combines two strings
+ * @dest: parameter
+ * @src: paramter
+ * @n: paramter
+ * Return: char
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a, b;
+
+	a = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	b = 0;
+	while (b < n && src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
